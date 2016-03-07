4ed54bbbd82d929a0420265ff09ac58cca5120b0
Pierre BONDOERFFER <pbondoer@student.42.fr>
2016-03-07 22:33:32 +0100

    change title too!

--- translate/locale.js
+++ translate/locale.js
@@ -56,6 +56,9 @@ function localeLoaded(src)
                 continue;
             }
             
+            if (key == "title")
+                document.title = json[key];
+            
             element.innerHTML = json[key];
         }
     }

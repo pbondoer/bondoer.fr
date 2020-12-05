de598e73664436ed9a76e2bc2efda4f94f554563
Pierre Bondoerffer <pierre@bondoer.fr>
2020-12-05 17:17:28 +0100

    remove custom config and rebuild

--- .htmlnanorc.js
+++ .htmlnanorc.js
@@ -1,29 +1,3 @@
 module.exports = {
-  sortAttributes: true,
-  collapseAttributeWhitespace: true,
-  collapseBooleanAttributes: {
-      amphtml: false,
-  },
-  custom: [],
-  deduplicateAttributeValues: true,
-  mergeScripts: true,
-  mergeStyles: true,
-  removeUnusedCss: true,
-  removeEmptyAttributes: true,
-  removeAttributeQuotes: false,
-  sortAttributesWithLists: 'alphabetical',
-  minifyUrls: false,
-  removeOptionalTags: false,
-
   collapseWhitespace: 'all',
-  removeComments: 'all',
-  removeAttributeQuotes: true,
-  removeRedundantAttributes: true,
-  removeUnusedCss: {},
-  minifyCss: {
-      preset: 'default',
-  },
-  minifySvg: {},
-  minifyConditionalComments: true,
-  removeOptionalTags: true,
 }
